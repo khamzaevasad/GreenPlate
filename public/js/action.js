26a3afc24ea1e61aa4fb61190db18074735f87ef
khamzaevasad <xamzayevasad442@gmail.com>
2025-09-29 05:12:00 +0900

    feat(search): implement search input functionality and connect it to the data rendering

--- public/js/action.js
+++ public/js/action.js
@@ -17,3 +17,18 @@ overlay.addEventListener("click", () => {
   sidebar.style.left = "-250px";
   overlay.style.display = "none";
 });
+
+// search function
+const searchInput = document.getElementById("search");
+const title = document.querySelectorAll(".card-heading");
+
+searchInput.addEventListener("input", () => {
+  const inputVal = searchInput.value.toLocaleLowerCase();
+  title.forEach((item) => {
+    if (item.textContent.toLocaleLowerCase().includes(inputVal)) {
+      item.closest(".card").style.display = "flex";
+    } else {
+      item.closest(".card").style.display = "none";
+    }
+  });
+});
